5b7bbee2bd33632477c6df231b81585a00fcb6c3
Aman <jainwalaman@gmail.com>
2022-04-01 23:18:22 -0700

    adding reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+//  if we want our app is predictable than we need make pure function
+export default function movies(state=[],action)// state is current state and and is action what we want to perform
+{
+if(action.type==='ADD_MOVIES')
+{
+    return action.movies;
+}
+return state;
+
+}
